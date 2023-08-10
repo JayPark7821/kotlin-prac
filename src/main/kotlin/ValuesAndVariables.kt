d7c01258377dd05e1afa34e190f52b66bce09251
jaypark <pjhyun1990@gmail.com>
2023-08-11 01:23:50 +0900

    Kotlin - 변수

---
+++ src/main/kotlin/ValuesAndVariables.kt
@@ -0,0 +1,17 @@
+var x = 5
+fun valuesAndVariables() {
+    x += 1
+    println(x)
+
+    val a : Int = 1
+    val b = 1
+    val c : Int
+    c = 3
+
+    var e : String = "Hello"
+    e = "World"
+
+//    var f = 123
+//    f = "error"
+
+}
