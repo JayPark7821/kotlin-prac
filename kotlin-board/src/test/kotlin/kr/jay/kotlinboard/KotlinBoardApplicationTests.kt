07e616e38f86059ff4846e35c044da7cf7052312
jaypark <pjhyun1990@gmail.com>
2023-09-05 18:40:40 +0900

    kotlin prac board - project setting

---
+++ kotlin-board/src/test/kotlin/kr/jay/kotlinboard/KotlinBoardApplicationTests.kt
@@ -0,0 +1,12 @@
+package kr.jay.kotlinboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
