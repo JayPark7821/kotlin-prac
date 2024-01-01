a56b06c0b72882ffbd3a23a405bc42ecc9f84f85
jaypark <pjhyun1990@gmail.com>
2024-01-01 22:41:45 +0900

    custom validator 정의

--- webflux-coroutine/src/main/kotlin/kr/jay/webfluxcoroutine/controller/AdvancedController.kt
+++ webflux-coroutine/src/main/kotlin/kr/jay/webfluxcoroutine/controller/AdvancedController.kt
@@ -1,7 +1,7 @@
 package kr.jay.webfluxcoroutine.controller
 
-import jakarta.validation.Valid
-import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.*
+import jakarta.validation.constraints.*
 import kr.jay.webfluxcoroutine.service.AdvancedService
 import mu.KotlinLogging
 import org.aspectj.lang.annotation.Aspect
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import kotlin.reflect.KClass
 
 /**
  * AdvancedController
@@ -41,6 +44,33 @@ class AdvancedController(
 
 data class ReqErrorTest(
     @field:NotEmpty
+    @field:Size(min = 1, max = 10)
     val id: String?,
+    @field:NotNull
+    @field:Positive(message = "age must be positive")
+    @field:Max(100)
     val age: Int?,
+    @field:DateString
+    val birthday: String?,
 )
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [DataValidator::class])
+annotation class DateString(
+    val message: String = "not a valid data",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = [],
+)
+
+class DataValidator : ConstraintValidator<DateString, String> {
+    override fun isValid(value: String?, context: ConstraintValidatorContext?): Boolean {
+        val text = value?.filter { it.isDigit() } ?: return true
+        return runCatching {
+            LocalDate.parse(text, DateTimeFormatter.ofPattern("yyyyMMdd")).let {
+                if (text != it.format(DateTimeFormatter.ofPattern("yyyyMMdd"))) null else true
+            }
+        }
+            .getOrNull() != null
+    }
+}
