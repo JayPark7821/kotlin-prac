a7432f223d2ea55a4fb74a07484c712bf3276785
jaypark <pjhyun1990@gmail.com>
2024-01-11 18:59:25 +0900

    init search prj

---
+++ search-engin-prac/src/main/kotlin/kr/jay/searchenginprac/SearchEnginPracApplication.kt
@@ -0,0 +1,11 @@
+package kr.jay.searchenginprac
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SearchEnginPracApplication
+
+fun main(args: Array<String>) {
+    runApplication<SearchEnginPracApplication>(*args)
+}
---
+++ search-engin-prac/src/test/kotlin/kr/jay/searchenginprac/SearchEnginPracApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.jay.searchenginprac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SearchEnginPracApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
