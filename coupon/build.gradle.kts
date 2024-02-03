ce85197bcc3005147172bcfe660bae3e0027c5d5
jaypark <pjhyun1990@gmail.com>
2024-02-03 10:57:55 +0900

    locust 테스트 환경 세팅

--- coupon/build.gradle.kts
+++ coupon/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22" apply false
     kotlin("plugin.jpa") version "1.8.22" apply false
+    kotlin("kapt") version "1.8.21"
     id("org.springframework.boot") version "3.1.5" apply false
     id("io.spring.dependency-management") version "1.1.3" apply false
 }
@@ -20,6 +21,7 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jetbrains.kotlin.plugin.spring")
     apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+    apply(plugin = "org.jetbrains.kotlin.kapt")
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
 
@@ -37,6 +39,10 @@ subprojects {
 
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+        kapt ("com.querydsl:querydsl-apt:5.0.0:jakarta")
+        kapt ("jakarta.annotation:jakarta.annotation-api")
+        kapt ("jakarta.persistence:jakarta.persistence-api")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
     }
 
