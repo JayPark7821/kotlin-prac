a963dedc6aca164abe87e2e52f64f7a27d7c3de1
jaypark <pjhyun1990@gmail.com>
2024-02-11 10:15:51 +0900

    모니터링 설정

--- coupon/build.gradle.kts
+++ coupon/build.gradle.kts
@@ -40,6 +40,10 @@ subprojects {
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+
+        implementation("org.springframework.boot:spring-boot-starter-actuator")
+        implementation("io.micrometer:micrometer-registry-prometheus")
+
         kapt ("com.querydsl:querydsl-apt:5.0.0:jakarta")
         kapt ("jakarta.annotation:jakarta.annotation-api")
         kapt ("jakarta.persistence:jakarta.persistence-api")
