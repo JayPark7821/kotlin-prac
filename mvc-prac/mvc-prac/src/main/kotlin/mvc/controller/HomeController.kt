72c5bf1414f34b95e2a8c733e3467e30cbd7a8a4
jaypark <pjhyun1990@gmail.com>
2024-02-24 21:19:05 +0900

    sample controller & page 추가.

---
+++ mvc-prac/mvc-prac/src/main/kotlin/mvc/controller/HomeController.kt
@@ -0,0 +1,17 @@
+package mvc.controller
+
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+/**
+ * HomeController
+ *
+ * @author jaypark
+ * @version 1.0.0
+ * @since 2/24/24
+ */
+class HomeController : Controller {
+    override fun handleRequest(request: HttpServletRequest, response: HttpServletResponse): String {
+        return "home.jsp"
+    }
+}
