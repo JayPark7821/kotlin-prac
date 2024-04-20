18cb1c8f72a54bd12d761311a90f418c8eca564c
jaypark <pjhyun1990@gmail.com>
2024-04-20 23:54:26 +0900

    reactive redis prac

--- webflux-coroutine/src/test/kotlin/kr/jay/webfluxcoroutine/example/RedisTemplateTest.kt
+++ webflux-coroutine/src/test/kotlin/kr/jay/webfluxcoroutine/example/RedisTemplateTest.kt
@@ -4,10 +4,20 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactor.awaitSingle
+import mu.KotlinLogging
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.domain.Range
+import org.springframework.data.redis.connection.DataType
+import org.springframework.data.redis.core.ReactiveListOperations
 import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.core.ReactiveZSetOperations
 import org.springframework.test.context.ActiveProfiles
+import java.util.*
+import kotlin.NoSuchElementException
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toJavaDuration
 
@@ -19,6 +29,9 @@ import kotlin.time.toJavaDuration
  * @version 1.0.0
  * @since 4/20/24
  */
+
+private val logger = KotlinLogging.logger {}
+
 @SpringBootTest
 @ActiveProfiles("test")
 class RedisTemplateTest(
@@ -47,6 +60,85 @@ class RedisTemplateTest(
         shouldThrow<NoSuchElementException> {
             ops.get(KEY).awaitSingle()
         }
+    }
 
+    "LinkedList"{
+        val ops = template.opsForList()
+        ops.rightPushAll(KEY, 2,3,4,5).awaitSingle()
+        template.type(KEY).awaitSingle() shouldBe DataType.LIST
+        ops.size(KEY).awaitSingle() shouldBe 4
+
+        for( i in 0 ..<ops.size(KEY).awaitSingle()){
+            ops.index(KEY, i).awaitSingle().let{
+                logger.debug { "$i: $it" }
+            }
+        }
+        ops.range(KEY, 0, -1).asFlow().collect { logger.debug { it } }
+        ops.range(KEY, 0, -1).toStream().forEach { logger.debug { it } }
+
+        ops.range(KEY, 0, -1).asFlow().toList() shouldBe listOf(2,3,4,5)
+
+        ops.rightPush(KEY, 6).awaitSingle()
+        ops.all(KEY) shouldBe listOf(2,3,4,5,6)
+
+        ops.leftPop(KEY).awaitSingle() shouldBe 2
+        ops.all(KEY) shouldBe listOf(3,4,5,6)
+
+        ops.leftPush(KEY, 9).awaitSingle()
+        ops.rightPop(KEY).awaitSingle() shouldBe 6
+        ops.all(KEY) shouldBe listOf(9,3,4,5)
+    }
+
+    "LinkedList LRU"{
+        val ops = template.opsForList()
+        ops.rightPushAll(KEY, 7,6,4,3,2,1,3).awaitSingle()
+
+        ops.remove(KEY, 0, 2).awaitSingle()
+        ops.all(KEY) shouldBe listOf(7,6,4,3,1,3)
+
+        ops.leftPush(KEY, 0, 2).awaitSingle()
+        ops.all(KEY) shouldBe listOf(2,7,6,4,3,1,3)
+    }
+
+    "hash"{
+        val ops = template.opsForHash<Int,String>()
+        val map = (1 .. 10).map {it to "val-$it"}.toMap()
+        ops.putAll(KEY, map).awaitSingle()
+
+        ops.size(KEY).awaitSingle() shouldBe 10
+        ops.get(KEY, 1).awaitSingle() shouldBe "val-1"
+        ops.get(KEY, 10).awaitSingle() shouldBe "val-10"
+    }
+
+    "sorted set"{
+        val ops = template.opsForZSet()
+        listOf(8,7,1,4,13,22,9,7,8).forEach {
+            ops.add(KEY, "$it", -0.1 * Date().time).awaitSingle()
+            ops.all(KEY).let {
+                logger.debug { it }
+            }
+        }
+        template.delete(KEY).awaitSingle()
+
+        listOf(
+            "jake" to 123,
+            "chulsoo" to 752,
+            "yeonghee" to 932,
+            "john" to 335,
+            "jake" to 623,
+        ).also{
+            it.toMap().toList().sortedBy { it.second }.let { logger.debug { it } }
+        }.forEach {
+            ops.add(KEY, it.first, it.second.toDouble()).awaitSingle()
+            ops.all(KEY).let { logger.debug { it } }
+        }
     }
 })
+
+suspend fun ReactiveListOperations<Any, Any>.all(key: Any): List<Any> {
+    return this.range(key, 0, -1).asFlow().toList()
+}
+
+suspend fun ReactiveZSetOperations<Any, Any>.all(key: Any): List<Any> {
+    return this.range(key, Range.closed(0, -1)).asFlow().toList()
+}
