671c51459f21dbd0027d4a234b0ae056e27c38bb
jaypark <pjhyun1990@gmail.com>
2024-04-23 19:29:01 +0900

    init PaymentApplication

---
+++ payment/src/main/kotlin/kr/jay/payment/PaymentApplication.kt
@@ -0,0 +1,11 @@
+package kr.jay.payment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaymentApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaymentApplication>(*args)
+}
---
+++ payment/src/test/kotlin/kr/jay/payment/PaymentApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.jay.payment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
