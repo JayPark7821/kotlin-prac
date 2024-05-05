5eeced3bfa828e9e76e055d8edfb381f2f91f1c1
jaypark <pjhyun1990@gmail.com>
2024-05-05 15:57:11 +0900

    testcontainer 추가 & Test fix

--- payment/build.gradle.kts
+++ payment/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.6.1")
     testImplementation("io.kotest:kotest-assertions-core:5.6.1")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+
+    testImplementation("org.testcontainers:testcontainers:1.19.0")
 }
 
 tasks.withType<KotlinCompile> {
